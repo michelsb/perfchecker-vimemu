291846c2a84f2ac5995aafee39e26225dda75623
Michel Bonfim <michelsb@gmail.com>
2019-09-06 16:59:02 -0300

    Remove lib dir and add scripts to run/stop

---
+++ stop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+for pid in $(pidof -x app.py); do
+    if [ $pid != $$ ]; then
+        kill -9 $pid
+    fi 
+done
